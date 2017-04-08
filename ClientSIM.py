a8a14a1da28db8816318b411011dfec05aea5cad
pathomphong <atomos_kung@hotmail.com>
2017-04-08 19:54:55 +0700

    no message

--- ClientSIM.py
+++ ClientSIM.py
@@ -1,7 +1,28 @@
 import socket
 import threading
 import sys
+
+from pyfirmata import Arduino, util
+from time import sleep
+import os
+board = Arduino('/dev/ttyS0')
+print "car driving simmulator"
+from pyfirmata import INPUT, OUTPUT, PWM, SERVO
+from time import sleep
+
+
+board = Arduino('/dev/ttyS0')
+it = util.Iterator(board)
+it.start()
 ################################################################################################
+whell = board.get_pin('a:0:i')  		#set a0 pin input whell
+accelerator = board.get_pin('a:1:i')	#set a1 pin input accellerator
+brake = board.get_pin('a:2:i')			#set a2 pin input brake
+
+p = board.get_pin('d:5:i')				#set d5 pin input gp
+r = board.get_pin('d:6:i')				#set d6 pin input gr
+n = board.get_pin('d:7:i')				#set d7 pin input gn
+d = board.get_pin('d:8:i')				#set d8 pin input gd
 
 #edittest
 PORT1 = 7769
@@ -74,32 +95,6 @@ if __name__ == '__main__':
 
 
 ################################################################################################################################################################
-
-
-
-from pyfirmata import Arduino, util
-from time import sleep
-import os
-board = Arduino('/dev/ttyS0')
-print "car driving simmulator"
-from pyfirmata import INPUT, OUTPUT, PWM, SERVO
-from time import sleep
-
-
-board = Arduino('/dev/ttyS0')
-it = util.Iterator(board)
-it.start()
-################################################################
-
-
-whell = board.get_pin('a:0:i')  		#set a0 pin input whell
-accelerator = board.get_pin('a:1:i')	#set a1 pin input accellerator
-brake = board.get_pin('a:2:i')			#set a2 pin input brake
-
-p = board.get_pin('d:5:i')				#set d5 pin input gp
-r = board.get_pin('d:6:i')				#set d6 pin input gr
-n = board.get_pin('d:7:i')				#set d7 pin input gn
-d = board.get_pin('d:8:i')				#set d8 pin input gd
 
 
 
