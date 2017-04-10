c2455eb76b8a9d021f2f89524f344180d9174526
pathomphong <atomos_kung@hotmail.com>
2017-04-11 00:04:03 +0700

    main run thread

--- ClientSIM.py
+++ ClientSIM.py
@@ -28,21 +28,17 @@ ACCELERATOR = 0
 BRAKE = 0
 
 
-
-
 IP = "192.168.100.1"
 PORT1 = 7769
 PORT2 = 7789
 
-
-
-COMMAND_SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-COMMAND_SERVER.connect((IP, PORT1))
 auth_message = "-a SIMULATOR_SET"
-COMMAND_SERVER.send(auth_message)
 
-DRIVER_SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-DRIVER_SERVER.connect((IP, PORT2))	
+
+
+
+
+
 
 
 def driverSocketResponse():
@@ -141,25 +137,31 @@ def readGear():	# Read if the button has been pressed.
 			GEAR = 'D'
 			data = '-cg ' + str(GEAR)
 			COMMAND_SERVER.send(data)
-	
+
+
+
 if __name__ == '__main__':
-	print "Start SIM !! "
-	SystemCommand("start SIM")
+	print "Start Server !! "
 
 
+	COMMAND_SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	COMMAND_SERVER.connect((IP, PORT1))
+	COMMAND_SERVER.send(auth_message)
 
-	# SIM Hardware part
+	DRIVER_SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	DRIVER_SERVER.connect((IP, PORT2))	
+	#read and sent
 	Read_Wheell_thread = threading.Thread(name = "Read_Wheel", target =readWheell)
 	Read_Accelerator_thread = threading.Thread(name = "Read_Accelerator", target = readAccelerator)
 	Read_Brake_thread = threading.Thread(name = "Read_Brake", target=readBrake)
 	Read_Gear_thread = threading.Thread(name = "Read_Gear", target =readGear)
-	
+
 	Read_Wheell_thread.start()
 	Read_Accelerator_thread.start()
 	Read_Brake_thread.start()
 	Read_Gear_thread.start()
-	
-	
+
+
 	#monitor
 	Monitor_Driver_thread = threading.Thread(target = driverSocketResponse)
 	Monitor_Command_thread = threading.Thread(target = commandSocketResponse)
